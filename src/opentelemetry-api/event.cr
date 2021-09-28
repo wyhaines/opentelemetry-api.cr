b18a2a314c694bfdecb8b5c16440c39d660b0cdd
Kirk Haines <wyhaines@gmail.com>
2021-09-28 09:23:39 -0600

    Yeah, there isn't a Time::Monotonic class. My subconscious thinks that there should be, though....

--- src/opentelemetry-api/event.cr
+++ src/opentelemetry-api/event.cr
@@ -4,7 +4,7 @@ module OpenTelemetry
   class Event
     property identifier : CSUUID = CSUUID.unique
     property name : String = ""
-    property timestamp : Time::Monotonic = Time.monotonic
+    property timestamp : Time::Span= Time.monotonic
     property wall_timestamp : Time = Time.utc
     property attributes : Hash(String, Attribute) = Hash(String, Attribute).new
     property parent_span : Span
