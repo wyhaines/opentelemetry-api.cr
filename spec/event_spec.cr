222fbdbce66ebdc9f85b9ecbd7bf8166320de68f
Kirk Haines <wyhaines@gmail.com>
2021-12-08 16:29:31 -0700

    Actually add the event spec.

---
+++ spec/event_spec.cr
@@ -0,0 +1,25 @@
+require "./spec_helper"
+
+describe OpenTelemetry::Event do
+  it "can create an event with direct initialization" do
+    event = OpenTelemetry::Event.new(
+      name: "test_event",
+      attributes: {
+        "test_attribute" => "test_value"
+      }
+    )
+    event.name.should eq "test_event"
+    event["test_attribute"].should eq "test_value"
+  end
+
+  it "can create an event with block based initialization" do
+    event = OpenTelemetry::Event.new do |e|
+      e.name = "test_event"
+      e.attributes = {
+        "test_attribute" => "test_value"
+      }
+    end
+    event.name.should eq "test_event"
+    event["test_attribute"].should eq "test_value"
+  end
+end
