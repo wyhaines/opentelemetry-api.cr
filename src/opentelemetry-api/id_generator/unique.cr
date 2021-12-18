91c7c043380cb18ba3253cec63e0e84327e4ab71
Kirk Haines <wyhaines@gmail.com>
2021-12-18 09:59:19 -0700

    IdGenerator now has specs.

--- src/opentelemetry-api/id_generator/unique.cr
+++ src/opentelemetry-api/id_generator/unique.cr
@@ -20,16 +20,16 @@ module OpenTelemetry
       @mutex.sync do
         t = Time.local
         increment_unique_identifier
-        seconds_buffer = Slice(UInt8).new(6, 0)
+        seconds_buffer = Slice(UInt8).new(8, 0)
         IO::ByteFormat::BigEndian.encode(t.internal_seconds, seconds_buffer)
-        span_bytes[0, 5].copy_from(seconds_buffer[1, 5])
+        span_bytes[0, 5].copy_from(seconds_buffer[3, 5])
         span_bytes[5, 3].copy_from(@unique_identifier)
       end
       span_bytes
     end
 
     # :nodoc:
-    def self.increment_unique_identifier
+    def increment_unique_identifier
       2.downto(0) do |position|
         new_byte_value = @unique_identifier[position] &+= 1
         break unless new_byte_value == 0
